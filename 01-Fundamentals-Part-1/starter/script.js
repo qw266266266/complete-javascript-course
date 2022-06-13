40e546884a83d1fb6976156ecf84488c4bc59a7c
qw266266266 <727033047@qq.com>
2022-06-13 18:24:08 +0800

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,6 @@
+/*
 let js = 'amazing';
-console.log(40 + 50 + 15);
+// console.log(40 + 50 + 15);
 
 console.log('Jonas');
 console.log(23);
@@ -7,4 +8,14 @@ console.log(23);
 let firstName = "Bob";
 console.log(firstName);
 
+// Variable name convention
 let jonas_matilda = "JM";
+let PI = 3.1415;
+
+let myFirstJob = "Programer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+*/
+
+console.log(true)
